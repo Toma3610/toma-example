8fa9890b6fd48997d65fe5f394c016e885d9698e
QiuQN <toma3610@163.com>
2020-12-16 14:13:18 +0800

    init

---
+++ toma-learning/lab-01-database-version-control/lab-01-database-version-control-flyway/src/main/java/com/toma/framework/lab01databaseversioncontrolflyway/Lab01DatabaseVersionControlFlywayApplication.java
@@ -0,0 +1,13 @@
+package com.toma.framework.lab01databaseversioncontrolflyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab01DatabaseVersionControlFlywayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab01DatabaseVersionControlFlywayApplication.class, args);
+    }
+
+}
---
+++ toma-learning/lab-01-database-version-control/lab-01-database-version-control-flyway/src/test/java/com/toma/framework/lab01databaseversioncontrolflyway/Lab01DatabaseVersionControlFlywayApplicationTests.java
@@ -0,0 +1,15 @@
+package com.toma.framework.lab01databaseversioncontrolflyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab01DatabaseVersionControlFlywayApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // 测试一下 2个仓库同时提交
+        //
+    }
+
+}
